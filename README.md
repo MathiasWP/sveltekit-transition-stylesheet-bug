27e84a5bf6a1f18edd2c9d40ad05cba3f7f89aa5
Mathias Picker <mattis123@live.no>
2022-02-11 20:00:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# sveltekit transition stylesheet bug
+
+1. Run npm i
+2. Run npm dev
+3. Open the dev-console and look at the elements
+4. Toggle visibility of element, see an empty stylesheet become appended in the head
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import {fly} from 'svelte/transition'
+let visible = false;
+</script>
+
+<button on:click={() => visible = !visible}>Toggle visibility</button>
+
+{#if visible}
+    <div transition:fly>Hello!</div>
+{/if}
